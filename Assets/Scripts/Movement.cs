62b9a9369b62ed9a9564060fa0f2c72e25d308ef
ToniYun <antonioyun2000@gmail.com>
2020-04-26 21:38:04 -0700

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public GameObject Plane;
+    public int shotCount = 3;
     public float enemySpeed = 10f;
     public static GameObject[] waypoints;
     Vector3 direction = Vector3.zero;
@@ -13,7 +15,6 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     private void Start()
     {
-       
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
         counter = Random.Range(0, 6);
 
@@ -27,7 +28,6 @@ public class Movement : MonoBehaviour
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
         GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * enemySpeed, direction.y * enemySpeed);
         transform.up = direction;
-
         if (Input.GetKeyDown(KeyCode.J))
         {
             random = !random;
@@ -36,6 +36,22 @@ public class Movement : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("Egg"))
+        {
+            Vector3 position = gameObject.transform.position;
+            shotCount--;
+            this.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, .25f * shotCount);
+
+            if (shotCount <= 0)
+            {
+                int ranX = Random.Range(-15, 16);
+                int ranY = Random.Range(-15, 16);
+
+                transform.position = new Vector3(position.x + ranX, position.y + ranY, position.z);
+                shotCount = 4;
+                this.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, .25f * shotCount);
+            }
+        }
         if (collision.gameObject.CompareTag("Waypoint") && random == false)
         {
             counter--;
